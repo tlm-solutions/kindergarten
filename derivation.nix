7832ff8e595143e6db37d766a6ce43fe05d10d1f
revol-xut <revol-xut@protonmail.com>
2023-02-24 14:16:31 +0100

    adding nix build

---
+++ derivation.nix
@@ -0,0 +1,33 @@
+{ pkgs, lib, config, domain, mkYarnPackage, yarn }:
+mkYarnPackage {
+    name = "kindergarten-compressed";
+    src = ./.;
+    packageJson = ./package.json;
+    yarnLock = ./yarn.lock;
+    
+    buildInputs = [ yarn ];
+    patchPhase = ''
+      substituteInPlace src/app/data/base/data.domain.ts \
+        --replace 'staging.dvb.solutions' '${domain}'
+    '';
+
+    buildPhase = ''
+      export NG_CLI_ANALYTICS=ci
+      cp -r $node_modules ./node_modules
+      chmod -R +w ./
+
+      FILE=$(readlink ./deps/kindergarten/node_modules)
+      rm ./deps/kindergarten/node_modules
+      mkdir ./deps/kindergarten/node_modules
+      cp -r $FILE/ ./deps/kindergarten/
+
+      yarn build
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp -r ./deps/kindergarten/dist/* $out/bin
+    '';
+
+    distPhase = "true";
+}
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+
+    utils = {
+      url = github:numtide/flake-utils;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          package-production = pkgs.callPackage ./derivation.nix {
+            domain = "dvb.solutions";
+          };
+          package-staging = pkgs.callPackage ./derivation.nix {
+            domain = "staging.dvb.solutions";
+          };
+        in
+        rec {
+          checks = packages;
+          packages = {
+            kindergarten = package-production;
+            kindergarten-staging = package-staging;
+            default = package-production;
+          };
+        }
+      ) // {
+      overlays.default = final: prev: {
+        inherit (self.packages.${prev.system})
+          kindergarten
+          kindergarten-staging;
+      };
+    };
+}
