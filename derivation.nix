8c271bb63228163eeb3e5c3b48ddb8101c0e67c1
revol-xut <revol-xut@protonmail.com>
2023-03-01 21:33:04 +0100

    fixing build

--- derivation.nix
+++ derivation.nix
@@ -2,9 +2,7 @@
 mkYarnPackage {
     name = "kindergarten";
     src = ./.;
-    packageJson = ./package.json;
-    yarnLock = ./yarn.lock;
-    
+
     buildInputs = [ yarn ];
     postPatch = ''
       substituteInPlace src/app/data/base/data.domain.ts \
@@ -12,17 +10,13 @@ mkYarnPackage {
     '';
 
     buildPhase = ''
-      cp -r $node_modules ./node_modules
-      chmod -R +w ./
-
       FILE=$(readlink ./deps/kindergarten/node_modules)
       rm ./deps/kindergarten/node_modules
       mkdir ./deps/kindergarten/node_modules
       cp -r $FILE/ ./deps/kindergarten/
-      cp -r ./node_modules/leaflet ./deps/kindergarten/node_modules/
+      cp -r ./node_modules/* ./deps/kindergarten/node_modules/
 
-      find | grep leaflet.css
-      ls deps/kindergarten/
+      ls ./deps/kindergarten/node_modules/
 
       yarn build
     '';
