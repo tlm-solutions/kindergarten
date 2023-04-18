08e5042e81d231e3d28992add9cd182bb1a598d9
Marcel <me@m4rc3l.de>
2023-04-18 23:05:03 +0200

    Improved landing page

--- src/app/pages/track/track-list/track-list.component.ts
+++ src/app/pages/track/track-list/track-list.component.ts
@@ -7,6 +7,8 @@ import {User, UserId} from "../../../data/user/user.domain";
 import {UserService} from "../../../data/user/user.service";
 import {IdHolder} from "../../../data/api.domain";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {formatDuration} from "../../../core/utils";
 
 @Component({
   selector: 'app-track-list',
@@ -21,7 +23,14 @@ export class TrackListComponent implements OnInit, OnDestroy {
     offset: new FormControl<number>(0, {nonNullable: true, validators: Validators.min(0)}),
     limit: new FormControl<number>(14, {nonNullable: true, validators: Validators.min(1)}),
   });
-  private readonly pagination = new BehaviorSubject({offset: 0, limit: 14});
+
+  private readonly pagination = this.route.queryParams.pipe(
+    map(({offset, limit}) => ({
+      offset: offset ?? 15,
+      limit: limit ?? 0
+    }))
+  );
+
   private readonly trackPages = this.pagination.pipe(
     switchMap(({offset, limit}) => this.trackService.getPage(offset, limit)),
     share(),
@@ -33,6 +42,8 @@ export class TrackListComponent implements OnInit, OnDestroy {
     private readonly trackService: TrackService,
     private readonly regionService: RegionService,
     private readonly userService: UserService,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
   ) {
   }
 
@@ -41,16 +52,16 @@ export class TrackListComponent implements OnInit, OnDestroy {
       .pipe(filter(() => this.form.valid))
       .subscribe(({offset, limit}) => {
         this.page.next((offset ?? 0) % (limit ?? 15) + 1);
-        this.pagination.next({
-          offset: offset ?? 0,
-          limit: limit ?? 15
+        this.router.navigate([], {
+          relativeTo: this.route,
+          queryParams: {offset, limit},
+          queryParamsHandling: "merge",
         })
       });
   }
 
   public ngOnDestroy(): void {
     this.subscription?.unsubscribe();
-    this.pagination.complete();
   }
 
   public prev(): void {
@@ -76,20 +87,4 @@ export class TrackListComponent implements OnInit, OnDestroy {
   protected duration(start: string, end: string): string {
     return formatDuration(Date.parse(end) - Date.parse(start));
   }
-}
-
-function formatDuration(ms: number): string {
-  ms = Math.abs(ms);
-
-  const time = {
-    day: Math.floor(ms / 86400000),
-    hour: Math.floor(ms / 3600000) % 24,
-    minute: Math.floor(ms / 60000) % 60,
-    second: Math.floor(ms / 1000) % 60,
-  };
-
-  return Object.entries(time)
-    .filter(val => val[1] !== 0)
-    .map(([key, val]) => `${val} ${key}${val !== 1 ? 's' : ''}`)
-    .join(', ');
 }
