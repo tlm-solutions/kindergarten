f0d6eed886229350a41286cf880a1c01854e17af
Marcel <me@m4rc3l.de>
2023-07-07 15:13:24 +0200

    Improved duration pipe

--- src/app/core/pipes/duration.pipe.ts
+++ src/app/core/pipes/duration.pipe.ts
@@ -16,37 +16,37 @@ export class DurationPipe implements PipeTransform {
     const absDiffInSecs = Math.abs(diffInSecs)
 
     if (absDiffInSecs < 60) {
-      return `${diffInSecs} ${diffInSecs === 1 ? $localize`second` : $localize`seconds`}`;
+      return `${diffInSecs} ${absDiffInSecs === 1 ? $localize`second` : $localize`seconds`}`;
     }
 
     if (absDiffInSecs >= 60 && absDiffInSecs < 3600) {
       const minutes = Math.trunc(diffInSecs / 60)
-      return `${minutes} ${minutes === 1 ? $localize`minute` : $localize`minutes`}`;
+      return `${minutes} ${Math.abs(minutes) === 1 ? $localize`minute` : $localize`minutes`}`;
     }
 
     if (absDiffInSecs > 3600 && absDiffInSecs < 86400) {
       const hours = Math.trunc(diffInSecs / 3600)
-      return `${hours} ${hours === 1 ? $localize`hour` : $localize`hours`}`;
+      return `${hours} ${Math.abs(hours) === 1 ? $localize`hour` : $localize`hours`}`;
     }
 
     if (absDiffInSecs >= 86400 && absDiffInSecs < 604800) {
       const days = Math.trunc(diffInSecs / 86400)
-      return `${days} ${days === 1 ? $localize`day` : $localize`days`}`;
+      return `${days} ${Math.abs(days) === 1 ? $localize`day` : $localize`days`}`;
     }
 
     if (absDiffInSecs >= 604800 && absDiffInSecs < 2592000) {
       const weeks = Math.trunc(diffInSecs / 604800)
-      return `${weeks} ${weeks === 1 ? $localize`week` : $localize`weeks`}`;
+      return `${weeks} ${Math.abs(weeks) === 1 ? $localize`week` : $localize`weeks`}`;
     }
 
     if (absDiffInSecs >= 2592000 && absDiffInSecs < 31536000) {
       const months = Math.trunc(diffInSecs / 2592000)
-      return `${months} ${months === 1 ? $localize`month` : $localize`months`}`;
+      return `${months} ${Math.abs(months) === 1 ? $localize`month` : $localize`months`}`;
     }
 
     if (absDiffInSecs >= 31536000) {
       const years = Math.trunc(diffInSecs / 31536000)
-      return `${years} ${years === 1 ? $localize`year` : $localize`years`}`;
+      return `${years} ${Math.abs(years) === 1 ? $localize`year` : $localize`years`}`;
     }
 
     return $localize`invalid data`;
