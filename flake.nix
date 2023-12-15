bc6625c3afcd437b4d21baf3db37df2f40dbee12
Marcel <me@m4rc3l.de>
2023-12-15 11:23:20 +0100

    Updated dependencies

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     pnpm2nix = {
       url = "github:nzbr/pnpm2nix-nzbr";
       inputs.nixpkgs.follows = "nixpkgs";
